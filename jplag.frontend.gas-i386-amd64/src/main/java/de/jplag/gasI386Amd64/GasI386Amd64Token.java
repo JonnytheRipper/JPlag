eb22a933ad6993d0c853dfad6b1473b0cadfe2cd
Jonny <joh.sch@gmx.at>
2021-11-26 14:46:56 +0100

    Create GasI386Amd64 token skeleton.

---
+++ jplag.frontend.gas-i386-amd64/src/main/java/de/jplag/gasI386Amd64/GasI386Amd64Token.java
@@ -0,0 +1,54 @@
+package de.jplag.gasI386Amd64;
+
+import de.jplag.Token;
+import de.jplag.TokenConstants;
+
+public class GasI386Amd64Token extends Token implements GasI386Amd64TokenConstants {
+	private static final long serialVersionUID = 7622542695302043164L;
+	private int column;
+	private int length;
+	private int line;
+	
+	public GasI386Amd64Token(int type, String file, int line, int column, int length) {
+		super(type, file, line, column, length);
+	}
+
+	@Override
+	public int getLine() {
+		return line;
+	}
+
+	@Override
+	public int getColumn() {
+		return column;
+	}
+
+	@Override
+	public int getLength() {
+		return length;
+	}
+	
+	@Override
+	public void setLine(int line) {
+		this.line = line;
+	}
+	
+	@Override
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	public static String type2String(int type) {
+		switch(type) {
+		case TokenConstants.FILE_END:
+			return "***EOF***";
+		default:
+			return "UNKNOWN: \"" + type + "\"";
+		}
+	}
+}
